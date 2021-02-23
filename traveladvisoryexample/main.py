e1ba711a600d3170fa9a0db839b6202e28b02a39
a.h.kadar <a.h.kadar@gmail.com>
2021-02-23 21:50:29 +0100

    moving files

---
+++ traveladvisoryexample/main.py
@@ -0,0 +1,15 @@
+import sys
+
+from jsonparser.json_parser import JSONParser
+from traveladvisoryexample.travel_advisory_parser import TravelAdvisoryParser
+
+def main():
+    url = "https://www.travel-advisory.info/api"
+    jsonobj = JSONParser(url)
+    traveobj = TravelAdvisoryParser(jsonobj.data)
+    for itr in range(1, len(sys.argv)):
+        country_name = traveobj.get_country_name(sys.argv[itr])
+        print(country_name, end = '\n')
+
+if __name__ == "__main__":
+    main()
